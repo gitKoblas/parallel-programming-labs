4e195fdda124e8fddb65de2fca4c04eb8c5dcc86
gitKoblas <Jarikpla@yandex.ru>
2026-04-24 17:20:26 +0400

    Готовая ЛР_1

--- solution/main.cpp
+++ solution/main.cpp
@@ -3,22 +3,26 @@
 #include <vector>
 #include <random>
 #include <chrono>
+#include <iomanip>
 
 using namespace std;
 using namespace std::chrono;
 
-
-vector<vector<double>> get_matrix(const string &filename) {
+vector<vector<double>> get_matrix(const string &filename)
+{
     ifstream in(filename);
-    if (!in.is_open()) {
+    if (!in.is_open())
+    {
         cout << "Can not open these file" << endl;
         return {};
     }
     int size;
     in >> size;
     vector<vector<double>> matrix(size, vector<double>(size));
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
             in >> matrix[i][j];
         }
     }
@@ -26,18 +30,22 @@ vector<vector<double>> get_matrix(const string &filename) {
     return matrix;
 }
 
-
-vector<vector<double>> matrix_miltiplier(const vector<vector<double>> & A, const vector<vector<double>> & B){
-    int A_size = A.size(); 
+vector<vector<double>> matrix_miltiplier(const vector<vector<double>> &A, const vector<vector<double>> &B)
+{
+    int A_size = A.size();
     int B_size = B.size();
-    if(A_size != B_size){
+    if (A_size != B_size)
+    {
         cout << "Can not multiply these matrix" << endl;
         return {};
     }
     vector<vector<double>> C(A_size, vector<double>(A_size, 0.0));
-    for (int i = 0; i < A_size; ++i) {
-        for (int j = 0; j < A_size; ++j) {
-            for (int k = 0; k < A_size; ++k) {
+    for (int i = 0; i < A_size; ++i)
+    {
+        for (int k = 0; k < A_size; ++k)
+        {
+            for (int j = 0; j < A_size; ++j)
+            {
                 C[i][j] += A[i][k] * B[k][j];
             }
         }
@@ -45,32 +53,39 @@ vector<vector<double>> matrix_miltiplier(const vector<vector<double>> & A, const
     return C;
 }
 
-vector<vector<double>> random_matrix(int size){
+vector<vector<double>> random_matrix(int size)
+{
     vector<vector<double>> matrix(size, vector<double>(size, 0.0));
     random_device rd;
     mt19937 gen(rd());
     uniform_real_distribution<double> distrib(-100, 100);
-    for(int i=0; i < size; ++i){
-        for(int j=0; j < size; ++j){
+    for (int i = 0; i < size; ++i)
+    {
+        for (int j = 0; j < size; ++j)
+        {
             matrix[i][j] = distrib(gen);
         }
     }
-    return matrix; 
+    return matrix;
 }
 
-
-void write_matrix(vector<vector<double>> matrix, const string &filename){
+void write_matrix(const vector<vector<double>> &matrix, const string &filename)
+{
     ofstream file(filename);
-    if (!file.is_open()) {
+    if (!file.is_open())
+    {
         cout << "Can not save in this directory" << endl;
         return;
     }
     int size = matrix.size();
     file << size << endl;
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            file << matrix[i][j];
-            if (j < size - 1) {
+    for (int i = 0; i < size; ++i)
+    {
+        for (int j = 0; j < size; ++j)
+        {
+            file << setprecision(15) << matrix[i][j];
+            if (j < size - 1)
+            {
                 file << " ";
             }
         }
@@ -78,13 +93,43 @@ void write_matrix(vector<vector<double>> matrix, const string &filename){
     }
 }
 
-int main(){
-    vector<vector<double>> matrix_1 = get_matrix("matrix1.txt");
-    vector<vector<double>> matrix_2 = get_matrix("matrix2.txt");
+void verification()
+{
+    system("py result_check.py");
+}
+
+void make_report(int size, const string &filename)
+{
+    ofstream file(filename, ios::app);
+    if (!file.is_open())
+    {
+        cout << "Can not write report in this directory" << endl;
+        return;
+    }
+    vector<vector<double>> matrix_1 = random_matrix(size);
+    vector<vector<double>> matrix_2 = random_matrix(size);
     auto start = high_resolution_clock::now();
     vector<vector<double>> result_matrix = matrix_miltiplier(matrix_1, matrix_2);
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
+    file << "Matrix size: " << size << endl;
+    file << "C++ Execution time: " << duration.count() << " microseconds" << endl;
+    file.close();
+    write_matrix(matrix_1, "matrix_1.txt");
+    write_matrix(matrix_2, "matrix_2.txt");
     write_matrix(result_matrix, "result.txt");
-    cout << "Execution time: " << duration.count() << " ms" << endl; 
+    verification();
+}
+
+int main()
+{
+    make_report(5, "report.txt");
+    make_report(10, "report.txt");
+    make_report(15, "report.txt");
+    make_report(20, "report.txt");
+    make_report(25, "report.txt");
+    make_report(50, "report.txt");
+    make_report(100, "report.txt");
+    make_report(500, "report.txt");
+    make_report(1000, "report.txt");
 }
--- solution/result_check.py
+++ solution/result_check.py
@@ -1,11 +1,44 @@
-def get_matrix:
+import numpy as np
+import time
 
 
+def get_matrix(filename: str) -> np.ndarray:
+    with open(filename, "r", encoding="utf-8") as file:
+        size = int(file.readline().strip())
+        matrix = []
+        for _ in range(size):
+            row = list(map(float, file.readline().split()))
+            matrix.append(row)
+    return np.array(matrix)
 
+
+def matrix_miltiplier(
+    matrix_1: np.ndarray, matrix_2: np.ndarray
+) -> tuple[np.ndarray, float]:
+    start = time.perf_counter()
+    result = np.dot(matrix_1, matrix_2)
+    stop = time.perf_counter()
+    execution_time = (stop - start) * (10**6)
+    return result, execution_time
+
+
+def make_report(filename: str, execution_time: float, status: bool) -> None:
+    with open(filename, "a", encoding="utf-8") as f:
+        f.write(f"NumPy Execution time: {execution_time:.2f} microseconds\n")
+        f.write(f"Verification: {status}\n")
+        f.write("-----------------------------------\n")
 
 
 def main():
-    
+    matrix_1 = get_matrix("matrix_1.txt")
+    matrix_2 = get_matrix("matrix_2.txt")
+    result_matrix = get_matrix("result.txt")
+    py_result, execution_time = matrix_miltiplier(matrix_1, matrix_2)
+    if np.allclose(result_matrix, py_result, atol=1e-3):
+        make_report("report.txt", execution_time, True)
+    else:
+        make_report("report.txt", execution_time, False)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
