399cfcc549c68f89648ed3f78364274870f69f97
gitKoblas <Jarikpla@yandex.ru>
2026-05-05 10:53:18 +0400

    Добавил технологию mpi

--- solution/main.cpp
+++ solution/main.cpp
@@ -1,133 +1,62 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
-#include <random>
+#include <fstream>
 #include <chrono>
-#include <iomanip>
 #include <mpi.h>
 
-using namespace std;
 using namespace std::chrono;
+using namespace std;
 
-vector<vector<double>> get_matrix(const string &filename)
-{
-    ifstream in(filename);
-    if (!in.is_open())
-    {
-        cout << "Can not open these file" << endl;
-        return {};
+
+vector<int> generate_matrice(int size) {
+    vector<int> matrice(size*size);
+    for (int i = 0; i < size * size; i++) {
+        matrice[i] = (rand() % 10000) / 100;
     }
-    int size;
-    in >> size;
-    vector<vector<double>> matrix(size, vector<double>(size));
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            in >> matrix[i][j];
-        }
-    }
-    in.close();
-    return matrix;
+    return matrice;
 }
 
-vector<vector<double>> matrix_miltiplier(const vector<vector<double>> &A, const vector<vector<double>> &B)
+int main(int argc, char** argv)
 {
-    int A_size = A.size();
-    int B_size = B.size();
-    if (A_size != B_size)
-    {
-        cout << "Can not multiply these matrix" << endl;
-        return {};
-    }
-    vector<vector<double>> C(A_size, vector<double>(A_size, 0.0));
-    for (int i = 0; i < A_size; ++i)
-    {
-        for (int k = 0; k < A_size; ++k)
-        {
-            for (int j = 0; j < A_size; ++j)
-            {
-                C[i][j] += A[i][k] * B[k][j];
+    int me, nprocs;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &me);
+    vector<int> sizes = { 200,400,800,1200,1600,2000 };
+    for (int size : sizes) {
+        vector<int> matrices_1(size * size);
+        vector<int> matrices_2(size * size);
+        matrices_1 = generate_matrice(size);
+        matrices_2=generate_matrice(size);
+        vector<int> matrice(size * size / nprocs);
+        vector<int> matrice_2(size * size,0);
+        if (me == 0) {
+            cout << "Matrix size = " << size << endl;
+            cout << "Execution time(microseconds): ";
+        }
+        MPI_Scatter(matrices_1.data(), size * size / nprocs, MPI_INT, matrice.data(), size * size / nprocs, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(matrices_2.data(), size * size, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Barrier(MPI_COMM_WORLD);
+        auto start = high_resolution_clock::now();
+        vector<int> result(size * size / nprocs,0);
+
+        int rows_per_proc = size / nprocs;
+        for (int i = 0; i < size / nprocs; i++) {
+            for (int j = 0; j < size; j++) {
+                int sum = 0;
+                for (int k = 0; k < size; k++) {
+                    sum += matrice[i * size + k] * matrices_2[k * size + j];
+                }
+                result[i * size + j] = sum;
             }
         }
-    }
-    return C;
-}
-
-vector<vector<double>> random_matrix(int size)
-{
-    vector<vector<double>> matrix(size, vector<double>(size, 0.0));
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_real_distribution<double> distrib(-100, 100);
-    for (int i = 0; i < size; ++i)
-    {
-        for (int j = 0; j < size; ++j)
-        {
-            matrix[i][j] = distrib(gen);
+        auto stop = high_resolution_clock::now();
+        MPI_Gather(result.data(), size * size / nprocs, MPI_INT, matrice_2.data(), size * size / nprocs, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Barrier(MPI_COMM_WORLD);
+        if (me == 0) {
+            double seconds = duration_cast<duration<double>>(stop - start).count();
+            cout << seconds << endl;
         }
     }
-    return matrix;
-}
-
-void write_matrix(const vector<vector<double>> &matrix, const string &filename)
-{
-    ofstream file(filename);
-    if (!file.is_open())
-    {
-        cout << "Can not save in this directory" << endl;
-        return;
-    }
-    int size = matrix.size();
-    file << size << endl;
-    for (int i = 0; i < size; ++i)
-    {
-        for (int j = 0; j < size; ++j)
-        {
-            file << setprecision(15) << matrix[i][j];
-            if (j < size - 1)
-            {
-                file << " ";
-            }
-        }
-        file << endl;
-    }
-}
-
-void make_report(int size, const string &filename)
-{
-    ofstream file(filename, ios::app);
-    if (!file.is_open())
-    {
-        cout << "Can not write report in this directory" << endl;
-        return;
-    }
-    vector<vector<double>> matrix_1 = random_matrix(size);
-    vector<vector<double>> matrix_2 = random_matrix(size);
-    auto start = high_resolution_clock::now();
-    vector<vector<double>> result_matrix = matrix_miltiplier(matrix_1, matrix_2);
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
-    file << "Matrix size: " << size << endl;
-    file << "C++ Execution time: " << duration.count() << " microseconds" << endl;
-    file.close();
-    write_matrix(matrix_1, "matrix_1.txt");
-    write_matrix(matrix_2, "matrix_2.txt");
-    write_matrix(result_matrix, "result.txt");
-}
-
-int main()
-{
-    make_report(5, "report.txt");
-    make_report(10, "report.txt");
-    make_report(15, "report.txt");
-    make_report(20, "report.txt");
-    make_report(25, "report.txt");
-    make_report(50, "report.txt");
-    make_report(100, "report.txt");
-    make_report(500, "report.txt");
-    make_report(1000, "report.txt");
-    make_report(1200, "report.txt");
-    make_report(1600, "report.txt");
-    make_report(2000, "report.txt");
+    MPI_Finalize();
 }
